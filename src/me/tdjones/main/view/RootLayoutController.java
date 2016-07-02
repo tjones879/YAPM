18159b0adb3d8306a70b253a87f68ec9be8fecca
Tyler Jones <jones@mines.edu>
2016-07-02 14:58:41 -0600

    Add handlers for dynamic nodes

--- src/me/tdjones/main/view/RootLayoutController.java
+++ src/me/tdjones/main/view/RootLayoutController.java
@@ -1,11 +1,15 @@
 package me.tdjones.main.view;
 
+import javafx.geometry.Bounds;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.shape.SVGPath;
+import me.tdjones.main.model.Episode;
 import me.tdjones.main.model.Feed;
-import me.tdjones.main.util.PlayMedia;
+import me.tdjones.main.util.MediaPlayerUtil;
+import me.tdjones.main.util.TimeUtil;
 
 import java.util.List;
 
@@ -15,59 +19,48 @@ import java.util.List;
 public class RootLayoutController {
 
     private final BorderPane rootBorderPane;
-    private Label currPlayTime;
-    private Label maxPlayTime;
+    private ImageView currPlayThumbnail;
+    private Label currPlayTitle;
+    private Label currPlayTime, maxPlayTime;
     private SVGPath skipBackButton, playPauseButton, skipForwardButton, volumeButton;
-    private ListView playingList;
+    private GridPane playingList;
+    private TilePane feedTilePane;
+    private ProgressBar progressBar;
+    private String unmutedSVG = "";
+    private String mutedSVG = "";
+    private String pauseSVG = "M 11,10 L 17,10 17,30 11,30 M 20,10 L 26,10 26,30 20,30";
+    private String playSVG = "M 10,10 L 10,30 L 30,20 z";
 
-    public RootLayoutController(List<Feed> feedList) {
+    public RootLayoutController() {
 
         rootBorderPane = new BorderPane();
-        rootBorderPane.setCenter(createFeedScrollPane(feedList));
+        rootBorderPane.setCenter(createFeedScrollPane());
         rootBorderPane.setBottom(createBottomHBox());
         rootBorderPane.setLeft(createLeftVBox());
 
     }
 
-    private ScrollPane createFeedScrollPane(List<Feed> feedList) {
+    private ScrollPane createFeedScrollPane() {
         ScrollPane feedScrollPane = new ScrollPane();
 
-        TilePane tilePane = new TilePane();
-
-        for (Feed feed : feedList) {
-            VBox vBox = new VBox();
-            vBox.setOnMouseClicked(event -> {
-                // TODO
-            });
-
-            ImageView feedThumbnail = new ImageView(feed.getThumbnail());
-            feedThumbnail.setPreserveRatio(true);
-            feedThumbnail.setFitHeight(250);
-
-            Label feedTitle = new Label(feed.getTitle());
-            feedTitle.setWrapText(true);
-
-            vBox.getChildren().addAll(feedThumbnail, feedTitle);
-            tilePane.getChildren().add(vBox);
-        }
-
-        feedScrollPane.setContent(tilePane);
+        feedTilePane = new TilePane();
+        feedScrollPane.setContent(feedTilePane);
         return feedScrollPane;
     }
 
     // TODO: Refactor createBottomHBox method.
     private HBox createBottomHBox() {
-        ImageView currPlayingThumbnail = new ImageView(PlayMedia.getCurrPlaying().getThumbnail());
-        currPlayingThumbnail.setPreserveRatio(true);
-        currPlayingThumbnail.setFitWidth(50);
+        currPlayThumbnail = createThumbnail(MediaPlayerUtil.getCurrPlaying().getThumbnail(), 50);
 
-        Label currPlayingFeed = new Label(PlayMedia.getCurrPlaying().getFeed().getTitle());
-        currPlayingFeed.setWrapText(true);
-        currPlayingFeed.setMaxWidth(150);
+        currPlayTitle = new Label(MediaPlayerUtil.getCurrPlaying().getTitle());
+        currPlayTitle.setWrapText(true);
+        currPlayTitle.setMaxWidth(150);
 
         AnchorPane progressNodes = new AnchorPane();
         // TODO: Remove hard-coded margins
-        ProgressBar progressBar = new ProgressBar();
+        progressBar = new ProgressBar();
+        progressBar.setOnMouseClicked(event -> handleProgressBarClick(event));
+
         progressNodes.setTopAnchor(progressBar, 2.0);
         progressNodes.setLeftAnchor(progressBar, 2.0);
         progressNodes.setRightAnchor(progressBar, 2.0);
@@ -82,39 +75,29 @@ public class RootLayoutController {
 
         skipBackButton = new SVGPath();
         skipBackButton.setContent("");
-        skipBackButton.setOnMouseClicked(event -> {
-            // TODO
-        });
+        skipBackButton.setOnMouseClicked(event -> handleSkipBackButton());
 
         playPauseButton = new SVGPath();
         playPauseButton.setContent("");
-        playPauseButton.setOnMouseClicked(event -> {
-            // TODO
-        });
+        playPauseButton.setOnMouseClicked(event -> handlePlayPauseButton());
 
         skipForwardButton = new SVGPath();
         skipForwardButton.setContent("");
-        skipForwardButton.setOnMouseClicked(event -> {
-            // TODO
-        });
+        skipForwardButton.setOnMouseClicked(event -> handleSkipForwardButton());
 
         volumeButton = new SVGPath();
-        volumeButton.setContent("");
-        volumeButton.setOnMouseClicked(event -> {
-            // TODO: Mute on click
-        });
-        volumeButton.setOnMouseEntered(event -> {
-            // TODO: Popup a volume slider
-        });
+        volumeButton.setContent(unmutedSVG);
+        volumeButton.setOnMouseClicked(event -> handleVolumeButtonClick());
+        volumeButton.setOnMouseEntered(event -> handleVolumeButtonEnter());
 
-        HBox hBox = new HBox(currPlayingThumbnail, currPlayingFeed, progressNodes, skipBackButton,
+        HBox hBox = new HBox(currPlayThumbnail, currPlayTitle, progressNodes, skipBackButton,
                              playPauseButton, skipForwardButton, volumeButton);
 
         return hBox;
     }
 
     // TODO: Refactor createLeftVBox method.
-    private VBox createLeftVBox(){
+    private VBox createLeftVBox() {
         SVGPath menuButton = new SVGPath();
         menuButton.setContent("");
 
@@ -127,12 +110,15 @@ public class RootLayoutController {
 
         // TODO
         GridPane menuGrid = new GridPane();
-        menuGrid.addRow();
-        menuGrid.addRow();
-        menuGrid.addRow();
+        menuGrid.addRow(0);
+        menuGrid.addRow(1);
+        menuGrid.addRow(2);
 
-        playingList = new ListView();
-
+        // TODO: Rename these variables
+        playingList = new GridPane();
+        ColumnConstraints thumbnailColumn = new ColumnConstraints();
+        thumbnailColumn.setPercentWidth(25);
+        playingList.getColumnConstraints().add(0, thumbnailColumn);
         ScrollPane playList = new ScrollPane(playingList);
 
         VBox vBox = new VBox();
@@ -140,4 +126,100 @@ public class RootLayoutController {
         return vBox;
     }
 
+    /**
+     * @param playingEpisodes List of episode objects.
+     */
+    public void updatePlayingList(List<Episode> playingEpisodes) {
+        int i = 0;
+        for (Episode episode : playingEpisodes) {
+            ImageView playingListThumbnail = createThumbnail(episode.getThumbnail(), 50);
+            Label playingListLabel = new Label(episode.getTitle());
+            // TODO: May create incredible amounts of rows, will have to test.
+            playingList.addRow(i, playingListThumbnail, playingListLabel);
+            i++;
+        }
+    }
+
+    public void updateFeedTilePane(List<Feed> feedList) {
+        for (Feed feed : feedList) {
+            VBox vBox = new VBox();
+            vBox.setOnMouseClicked(event -> {
+                // TODO
+            });
+
+            ImageView feedThumbnail = createThumbnail(feed.getThumbnail(), 250);
+
+            Label feedTitle = new Label(feed.getTitle());
+            feedTitle.setWrapText(true);
+
+            vBox.getChildren().addAll(feedThumbnail, feedTitle);
+            feedTilePane.getChildren().add(vBox);
+        }
+    }
+
+    private ImageView createThumbnail(String imageUrl, int imageWidth) {
+        ImageView thumbnail = new ImageView(imageUrl);
+        thumbnail.setPreserveRatio(true);
+        thumbnail.setFitWidth(imageWidth);
+
+        return thumbnail;
+    }
+
+    private void handleSkipBackButton() {
+        MediaPlayerUtil.skipBack();
+    }
+
+    private void handlePlayPauseButton() {
+        if (MediaPlayerUtil.isPlaying()) {
+            MediaPlayerUtil.play();
+            playPauseButton.setContent(pauseSVG);
+        } else {
+            MediaPlayerUtil.pause();
+            playPauseButton.setContent(playSVG);
+        }
+    }
+
+    private void handleSkipForwardButton() {
+        MediaPlayerUtil.skipForward();
+    }
+
+    private void handleVolumeButtonClick() {
+        if (MediaPlayerUtil.toggleMute()) {
+            volumeButton.setContent(mutedSVG);
+        } else {
+            volumeButton.setContent(unmutedSVG);
+        }
+    }
+
+    private void handleVolumeButtonEnter() {
+        // TODO: Popup a volume slider
+    }
+
+    private void handleProgressBarClick(MouseEvent mouseEvent) {
+        double mouseX = mouseEvent.getX();
+        Bounds bounds = progressBar.getLayoutBounds();
+        double seekTime = ((mouseX - bounds.getMinX()) / bounds.getMaxX()) * 1000
+                            * MediaPlayerUtil.getCurrPlaying().getLength();
+        MediaPlayerUtil.seek(seekTime);
+    }
+
+    public void updateEpisodeNodes(){
+        Episode episode = MediaPlayerUtil.getCurrPlaying();
+
+        currPlayThumbnail = createThumbnail(episode.getThumbnail(), 50);
+        currPlayTitle.setText(episode.getTitle());
+        currPlayTime.setText("0:00");
+        maxPlayTime.setText(TimeUtil.formatTime(episode.getLength()));
+    }
+
+    public void updateProgressBar(){
+            int currTime = (int) MediaPlayerUtil.getCurrTime().toSeconds();
+            long maxTime = MediaPlayerUtil.getCurrPlaying().getLength();
+            progressBar.setProgress(currTime / maxTime);
+            currPlayTime.setText(TimeUtil.formatTime(currTime));
+    }
+
+    public void updateCurrPlayTime(){
+        currPlayTime.setText(TimeUtil.formatTime((int) MediaPlayerUtil.getCurrTime().toSeconds()));
+    }
 }
