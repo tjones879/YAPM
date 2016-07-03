95ba8c03a9254bdf965b75ffb098e7036cf05f32
Tyler Jones <jones@mines.edu>
2016-07-03 12:35:25 -0600

    Unmarshal support with JAXB with validation.

--- src/me/tdjones/main/util/XMLUtil.java
+++ src/me/tdjones/main/util/XMLUtil.java
@@ -1,7 +1,48 @@
 package me.tdjones.main.util;
 
-/**
- * Created by Tyler on 6/30/2016.
- */
+import me.tdjones.main.model.Episode;
+import me.tdjones.main.model.Feed;
+import me.tdjones.main.model.RSS;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
 public class XMLUtil {
+    public static Feed parseRSS(URL feedURL){
+        try {
+            JAXBContext jc = JAXBContext.newInstance(RSS.class);
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            RSS rss = (RSS) unmarshaller.unmarshal(feedURL.openStream());
+            validateRss(rss);
+            return rss.getFeed();
+        }catch (JAXBException e){
+            e.printStackTrace();
+            return null;
+        }catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void validateRss(RSS rss){
+        validateFeed(rss.getFeed());
+        for (Episode episode : rss.getFeed().getEpisodeList()){
+            episode.setFeed(rss.getFeed());
+            validateEpisode(episode);
+        }
+    }
+
+    private static void validateFeed(Feed feed){
+
+    }
+
+    private static void validateEpisode(Episode episode){
+        if (episode.getThumbnail().getUrl() == null){
+            episode.setThumbnail(episode.getFeed().getThumbnail());
+        }
+    }
 }
