b94797559931a9413c0d249774657a2490abb3f7
Tyler Jones <jones@mines.edu>
2016-07-03 12:32:38 -0600

    Add formatting and parsing for timestamps

--- src/me/tdjones/main/util/TimeUtil.java
+++ src/me/tdjones/main/util/TimeUtil.java
@@ -1,7 +1,31 @@
 package me.tdjones.main.util;
 
-/**
- * Created by Tyler on 6/30/2016.
- */
 public class TimeUtil {
+    public static String formatTime(int seconds){
+        if (seconds > 3600){
+            return (String.format("%d:%02d:%02d", seconds/3600, (seconds%3600)/60, seconds%60));
+        } else if (seconds < 3600 && seconds > 60) {
+            return String.format("%d:%02d", seconds/60, seconds%60);
+        } else {
+            return String.format("%d:%02d", seconds/60, seconds%60);
+        }
+    }
+
+    public static int parseTime(String length){
+        String[] values = length.split(":");
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 0;
+        if(values.length == 3){
+            hours = Integer.parseInt(values[0]);
+            minutes = Integer.parseInt(values[1]);
+            seconds = Integer.parseInt(values[2]);
+        }else if (values.length == 2){
+            minutes = Integer.parseInt(values[0]);
+            seconds = Integer.parseInt(values[1]);
+        }else if (values.length == 1){
+            seconds = Integer.parseInt(values[0]);
+        }
+        return hours*3600 + minutes*60 + seconds;
+    }
 }
--- src/me/tdjones/main/view/FeedLayoutController.java
+++ src/me/tdjones/main/view/FeedLayoutController.java
@@ -1,7 +1,4 @@
 package me.tdjones.main.view;
 
-/**
- * Created by Tyler on 6/30/2016.
- */
 public class FeedLayoutController {
 }
