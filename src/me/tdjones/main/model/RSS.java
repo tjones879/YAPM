838985b49a56edc6a8c8cbf2261dcdfd3f558e77
Tyler Jones <jones@mines.edu>
2016-07-02 20:40:51 -0600

    Add RSS model for unmarshalling

--- src/me/tdjones/main/model/RSS.java
+++ src/me/tdjones/main/model/RSS.java
@@ -1,7 +1,16 @@
 package me.tdjones.main.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * Created by Tyler on 6/30/2016.
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name="rss")
 public class RSS {
+    @XmlElement(name = "channel")
+    Feed feed;
 }
