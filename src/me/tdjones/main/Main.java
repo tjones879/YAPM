c03f7551c5e62b7e8010480af8e0ae2522cb6a5b
Tyler Jones <jones@mines.edu>
2016-08-25 17:08:55 -0600

    Add support for subscriptions with agnostic system

--- src/me/tdjones/main/Main.java
+++ src/me/tdjones/main/Main.java
@@ -1,7 +1,6 @@
 package me.tdjones.main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.media.Media;
@@ -17,23 +16,30 @@ import me.tdjones.main.view.RootLayoutController;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 
 public class Main extends Application{
 
     private static Stage primaryStage;
     private BorderPane musicMain;
-    private File directory;
+    private static File subscriptionDir;
     private Playlist nowPlaying;
     private MediaPlayer mediaPlayer;
     private Scene scene;
     private RootLayoutController rootLayoutController;
     private FeedLayoutController feedLayoutController;
+    private static List<Feed> subscriptions;
     private Feed feed;
 
     public final static String itunesNamespace = "http://www.itunes.com/dtds/podcast-1.0.dtd";
 
     public static void main(String[] args) {
+        subscriptionDir = getSubscriptionDir();
+        importSubscriptions();
+
         Application.launch(Main.class);
     }
 
@@ -48,13 +54,39 @@ public class Main extends Application{
         MediaPlayerUtil.setCurrPlaying(feed.getEpisodeList().get(0));
         mediaPlayer = new MediaPlayer(new Media("http://feeds.soundcloud.com/stream/271124645-beautiful-anonymous-17-early-onset.mp3"));
         initLayout();
+        rootLayoutController.updateFeedTilePane(subscriptions);
     }
 
     private void initLayout(){
         rootLayoutController = new RootLayoutController();
 
-        scene = new Scene(rootLayoutController.getRootBorderPane(), 400, 600);
+        scene = new Scene(rootLayoutController.getRootBorderPane(), 900, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private static void importLibrary(){
+    }
+
+    private static void importSubscriptions(){
+        subscriptions = new ArrayList<>();
+        List<URL> RSSUrls = XMLUtil.parseOPML(subscriptionDir);
+        for (URL feedURL : RSSUrls){
+            subscriptions.add(XMLUtil.parseRSS(feedURL));
+        }
+    }
+
+    private static File getSubscriptionDir(){
+        String directory = new String();
+        String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+        if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0) || (OS.indexOf("nux") >= 0)){
+            directory = System.getProperty("user.home") + File.separator + "YAPM" + File.separator + "subscriptions.opml";
+        } else if (OS.indexOf("win") >= 0) {
+            directory = System.getProperty("user.home") + File.separator + "Documents" + File.separator + "YAPM" + File.separator + "subscriptions.opml";
+        } else {
+            // TODO: Handle other OS or error cases
+            directory = "";
+        }
+        return new File(directory);
     }
 }
